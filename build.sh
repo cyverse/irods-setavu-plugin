7ed756b5f3ac8889e5f8eb91a068ea737a8a08ec
Tony Edgin <tedgin@iplantcollaborative.org>
2016-08-19 14:46:28 -0700

    builds as current user instead of root

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 docker build --tag irods-dev-build .
-docker run --rm --tty --volume=$(pwd)/src:/src --name=setavu-builder \
+docker run --rm --tty --user=$(id -u):$(id -g) --volume=$(pwd)/src:/src --name=setavu-builder \
 	   irods-dev-build make clean all 
