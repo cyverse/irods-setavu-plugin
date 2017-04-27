03812a3a73c699b596120feb2b560fdb1ee16b81
Tony Edgin <tedgin@iplantcollaborative.org>
2017-04-27 11:51:38 -0700

    DS-198: added build for CentOS 7

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
-docker build --file dockerfiles/Dockerfile.centos6 --tag irods-dev-build:4.1.10-centos6 .
-docker run --rm --tty --user=$(id -u):$(id -g) --volume=$(pwd)/src:/src --name=setavu-builder \
-	   irods-dev-build:4.1.10-centos6 make clean all
-mkdir --parents libraries/centos6
-mv src/libmsiSetAVU.so libraries/centos6
+for os in centos6 centos7
+do
+  docker build --file dockerfiles/Dockerfile."$os" --tag irods-dev-build:4.1.10-"$os" .
+
+  docker run --rm --tty --user=$(id -u):$(id -g) --volume=$(pwd)/src:/src --name=setavu-builder \
+	           irods-dev-build:4.1.10-"$os" make clean all
+
+  mkdir --parents libraries/"$os"
+  mv --force src/libmsiSetAVU.so libraries/"$os"
+done
