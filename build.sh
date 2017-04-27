e21cfe615094d9084119b9ebb4b40ba7c8f26175
Tony Edgin <tedgin@iplantcollaborative.org>
2017-04-27 11:07:40 -0700

    DS-198: Reorg in prep for supporting mult OSes

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
-docker build --tag irods-dev-build .
+docker build --file dockerfiles/Dockerfile.centos6 --tag irods-dev-build:4.1.10-centos6 .
 docker run --rm --tty --user=$(id -u):$(id -g) --volume=$(pwd)/src:/src --name=setavu-builder \
-	   irods-dev-build make clean all 
+	   irods-dev-build:4.1.10-centos6 make clean all
+mkdir --parents libraries/centos6
+mv src/libmsiSetAVU.so libraries/centos6
