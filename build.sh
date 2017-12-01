7d7eda491ded6d37a8c68d6b9c4e2c9d3ad40686
Tony Edgin <tedgin@iplantcollaborative.org>
2017-12-01 15:52:39 -0700

    DS-217 converted body of build.sh to function

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #! /bin/sh
 
-for os in centos6 centos7
-do
-  docker build --file dockerfiles/Dockerfile."$os" --tag irods-dev-build:4.1.10-"$os" .
 
-  docker run --rm --tty --user=$(id -u):$(id -g) --volume=$(pwd)/src:/src --name=setavu-builder \
-	           irods-dev-build:4.1.10-"$os" make clean all
+main()
+{
+  for os in centos6 centos7
+  do
+    docker build --file dockerfiles/Dockerfile."$os" --tag irods-dev-build:4.1.10-"$os" .
 
-  mkdir --parents libraries/"$os"
-  mv --force src/libmsiSetAVU.so libraries/"$os"
-done
+    docker run --rm --tty --user=$(id -u):$(id -g) --volume=$(pwd)/src:/src --name=setavu-builder \
+	             irods-dev-build:4.1.10-"$os" make clean all
+
+    mkdir --parents libraries/"$os"
+    mv --force src/libmsiSetAVU.so libraries/"$os"
+  done
+}
+
+
+set -e
+
+main
